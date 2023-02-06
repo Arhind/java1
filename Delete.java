b00aefb915e4ac18b560972d80ac07f73dd59e0f
Arhind Kumar Gautam <arhindg@gmail.com>
2023-02-06 10:59:10 +0530

    Add files via upload

---
+++ Delete.java
@@ -0,0 +1,17 @@
+package coding;
+
+public class Delete {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a=true;
+		boolean b=true;
+		boolean c=true;
+		
+		if( !a || !b || !c)
+			System.out.println(true);
+		else
+			System.out.println(false);
+	}
+
+}
